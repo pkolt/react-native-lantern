3004b6c4037ceb60c662349eacedf52a75caa9d0
pkolt <pkoltyshev@gmail.com>
2020-05-05 18:06:08 +0500

    fix api version

--- android/src/main/java/com/reactnative/lantern/ReactNativeLanternModule.java
+++ android/src/main/java/com/reactnative/lantern/ReactNativeLanternModule.java
@@ -7,23 +7,48 @@ import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 
-import android.os.Build;
-import android.net.ssl;
+import android.content.Context;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraManager;
 
 public class ReactNativeLanternModule extends ReactContextBaseJavaModule {
 
   private final ReactApplicationContext reactContext;
-  private LanternBase lantern;
+  private CameraManager camManager;
+  private String camId;
+  private boolean turnState = false;
 
   public ReactNativeLanternModule(ReactApplicationContext reactContext) {
     super(reactContext);
     this.reactContext = reactContext;
 
     // >= API 23 (Android 6.0)
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-      lantern = new Lantern23(reactContext);
-    } else {
-      lantern = new Lantern16();
+    try {
+      camManager = (CameraManager) reactContext.getSystemService(Context.CAMERA_SERVICE);
+      camId = findCameraId();
+
+      CameraManager.TorchCallback torchCallback = new CameraManager.TorchCallback() {
+        @Override
+        public void onTorchModeChanged(String id, boolean enabled) {
+          super.onTorchModeChanged(id, enabled);
+          if (camId.equals(id)) {
+              turnState = enabled;
+          }
+        }
+
+        @Override
+        public void onTorchModeUnavailable(String id) {
+          super.onTorchModeUnavailable(id);
+          if (camId.equals(id)) {
+              camId = findCameraId();
+          }
+        }
+      };
+
+      // fires onTorchModeChanged upon register
+      camManager.registerTorchCallback(torchCallback, null);
+    } catch (Exception e) {
+      e.printStackTrace();
     }
 
     LifecycleEventListener lifecycleEventListener = new LifecycleEventListener() {
@@ -35,35 +60,56 @@ public class ReactNativeLanternModule extends ReactContextBaseJavaModule {
 
       @Override
       public void onHostDestroy() {
-        lantern.turnOff();
+
       }
     };
 
     reactContext.addLifecycleEventListener(lifecycleEventListener);
   }
 
+  private String findCameraId() {
+    try {
+      String[] camIds = camManager.getCameraIdList();
+      for (String camId : camIds) {
+        CameraCharacteristics characteristics = camManager.getCameraCharacteristics(camId);
+        boolean isAvailableFlash = characteristics.get(CameraCharacteristics.FLASH_INFO_AVAILABLE);
+        if (isAvailableFlash) {
+          return camId;
+        }
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    return null;
+  }
+
   @ReactMethod
   private void turn(boolean state) {
-    lantern.turn(state);
+    try {
+      camManager.setTorchMode(camId, state);
+      turnState = state;
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
   }
 
   @ReactMethod
   public void turnOn() {
-    lantern.turnOn();
+    turn(true);
   }
 
   @ReactMethod
   public void turnOff() {
-    lantern.turnOff();
+    turn(false);
   }
 
   @ReactMethod
   public void requestTurnState(Callback cb) {
-    cb.invoke(lantern.getTurnState());
+    cb.invoke(turnState);
   }
 
   @Override
   public String getName() {
-    return "RNReactNativeLantern";
+    return "ReactNativeLantern";
   }
 }
